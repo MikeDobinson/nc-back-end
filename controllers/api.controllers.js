8d76da868db4544ec794f5345bbc2388783cf8dd
Mike <michael.dobinson@gmail.com>
2023-04-20 10:01:15 +0100

    fix api controller

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -2,7 +2,6 @@ const fs = require('fs/promises');
 
 exports.getApi = (req, res) => {
   fs.readFile(`${__dirname}/../endpoints.json`, 'utf-8').then((data) => {
-    console.log('data :>> ', data);
     res.status(200).send(data);
   });
 };
