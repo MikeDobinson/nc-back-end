4b3fc5c94d8ec06511983eb654ff41617ff07ce4
Mike <michael.dobinson@gmail.com>
2023-04-20 03:08:56 +0100

    added comment count to the individual article object

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,7 +3,17 @@ const format = require('pg-format');
 
 exports.fetchArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(
+      `
+    SELECT articles.*, count(comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id=comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id
+    
+    `,
+      [article_id]
+    )
     .then((result) => {
       if (result.rows.length === 0) {
         return Promise.reject({ status: 404, msg: 'Article not found' });
@@ -54,20 +64,6 @@ exports.fetchAllArticles = (topic, order = 'DESC', sort_by = 'created_at') => {
     }
   });
 };
-
-//   return db
-//     .query(
-//       `SELECT articles.*, count(comments.comment_id) AS comment_count
-//       FROM articles
-//       LEFT JOIN comments ON articles.article_id = comments.article_id
-//       GROUP BY articles.article_id
-//       ORDER BY articles.created_at DESC;
-//   `
-//     )
-//     .then((result) => {
-//       return result.rows;
-//     });
-// };
 
 exports.editArticleById = (article_id, inc_votes) => {
   if (!inc_votes) inc_votes = 0;
